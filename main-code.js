450471952b01b46ebd3d3fd5120b7256a41374c5
noambenshoham <noambenshoham@gmail.com>
2022-04-30 23:50:25 +0300

    Basic board

---
+++ main-code.js
@@ -0,0 +1,26 @@
+const boardEl = document.createElement("table");
+const BOARD_SIZE = 8;
+
+function createCheckersBoard() {
+    boardEl.classList.add("checkersBoard");
+    document.body.appendChild(boardEl);
+    for (let row = 0; row < BOARD_SIZE; row++) {
+        let rowElement = boardEl.insertRow();
+        for (let col = 0; col < BOARD_SIZE; col++) {
+            let cellElement = rowElement.insertCell();
+            if ((row + col) % 2 === 0) { // Zig-Zag 
+                cellElement.classList.add("white");
+            } else {
+                cellElement.classList.add("black");
+            }
+            cellElement.addEventListener('click', () => onCellClick(row, col));
+        }
+    }
+    // boardData = new BoardData('white_player');
+}
+
+window.addEventListener('load', createCheckersBoard);
+
+function onCellClick(row, col) {
+    console.log("clicked")
+}   
